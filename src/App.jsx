179b0bb5a74b78e345862d5c5de52f90423a6d98
BennyZHill <ben.h@builtbypixel.com>
2021-07-28 13:46:05 +0100

    git commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css';
+import Card from './components/cards'
+
+function App () {
+  return (
+    <div className='card-wrapper'>
+      <Card
+      title=''
+      imageUrl=''
+      body=''
+      logo='N'
+      />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/cards.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import './cards.css' 
+
+
+
+function Card({imageUrl,body,logo}) {
+    return (
+    <div className='card-container'>
+        <div className="card1">
+       <div className='image-container'>
+            <img src= {imageUrl} alt='' />
+        </div>
+        <div className='card-title'>
+            <h1>marvel <span>A</span>vengers</h1>
+         </div>
+         <div className='card-body'>
+             {body}
+        </div>
+        <div className="logo">{logo}</div>
+        </div>
+
+        <div className="card2">
+       <div className='image-container'>
+            <img src= {imageUrl} alt='' />
+        </div>
+        <div className='card-title'>
+            <h2>the witcher</h2>
+         </div>
+         <div className='card-body'>
+             {body}
+        </div>
+        <div className="logo">{logo}</div>
+        </div>
+
+        
+        <div className="card3">
+       <div className='image-container'>
+            <img src= {imageUrl} alt='' />
+        </div>
+        <div className='card-title'>
+           <h3>A silent voice</h3>
+         </div>
+         <div className='card-body'>
+             {body}
+        </div>
+        <div className="logo">{logo}</div>
+        </div>
+    </div>
+    )
+}
+
+
+export default Card
